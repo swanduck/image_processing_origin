aee010fcc1b060e6e0460c4e8a081e185acf3216
swanduck <skarsty930@gmail.com>
2023-12-05 00:04:20 +0700

    new testing image, par is slower
    par_invert turns out to be slower since the seq version seems to already be quite optimized.

--- src/invert/mod.rs
+++ src/invert/mod.rs
@@ -26,29 +26,15 @@ pub fn invert(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
 
 #[allow(dead_code)]
 pub fn par_invert(img: &DynamicImage) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
-    let (width, height) = img.dimensions();
+    let mut output_img = img.clone().to_rgba8();
 
-    let output_img: Vec<_> = (0..height).into_par_iter().map(|y| {
-        let mut row = vec![Rgba([0, 0, 0, 0]); width as usize];
-        for x in 0..width {
-            let pixel = img.get_pixel(x, y);
-            let inverted_pixel = Rgba([
-                255 - pixel[0],
-                255 - pixel[1],
-                255 - pixel[2],
-                pixel[3],  // alpha channel is usually left unchanged
-            ]);
-            row[x as usize] = inverted_pixel;
-        }
-        row
-    }).collect();
+    output_img
+        .par_chunks_mut(4)
+        .for_each(|pixel| {
+            pixel[0] = 255 - pixel[0];
+            pixel[1] = 255 - pixel[1];
+            pixel[2] = 255 - pixel[2];
+        });
 
-    let mut output_img_buffer = ImageBuffer::new(width, height);
-    for (y, row) in output_img.iter().enumerate() {
-        for (x, &pixel) in row.iter().enumerate() {
-            output_img_buffer.put_pixel(x as u32, y as u32, pixel);
-        }
-    }
-
-    output_img_buffer
+    output_img
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod invert;
 use image::io::Reader as ImageReader;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>  {
-    let img = ImageReader::open("burger.png")?.decode()?;
+    let img = ImageReader::open("wallpaper.png")?.decode()?;
     use std::time::Instant;
     // use pixelate::*;
     // let img_ = pixelate(&img, (20, 20));
